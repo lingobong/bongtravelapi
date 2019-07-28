920497d00cf3fb75f252455de3d3eebc73d9194c
jaewan <jw.ryu@plingo.io>
2019-07-28 23:28:54 +0900

    소셜 추가 완료

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const { expressAutoReloader, expressAuthorization } = require('../services')
 router.use('/upload', expressAuthorization.authRequired, expressAutoReloader(__dirname, './upload'))
 router.use('/users', expressAutoReloader(__dirname, './users'))
 router.use('/travels', expressAuthorization.authRequired, expressAutoReloader(__dirname, './travels'))
-router.use('/socials', expressAuthorization.authRequired, expressAutoReloader(__dirname, './socials'))
+router.use('/socials', expressAuthorization.injectUser, expressAutoReloader(__dirname, './socials'))
 router.use('/login', expressAutoReloader(__dirname, './login'))
 
 
--- src/routes/socials/index.js
+++ src/routes/socials/index.js
@@ -1,6 +1,40 @@
 const express = require('express')
 const router = express.Router()
+const models = require('../../models')
+const mongoose = require('mongoose')
 
-
+router.get('/latest', async (req, res) => {
+     let { lastId } = req.query
+     let aggregate = []
+     if (!!lastId) {
+          aggregate.push({
+               $match: {
+                    _id: {
+                         $lt: mongoose.Types.ObjectId(lastId)
+                    }
+               }
+          })
+     }
+     aggregate.push({
+          $sort: {
+               _id: -1,
+          }
+     })
+     aggregate.push({
+          $limit: 20,
+     })
+     aggregate.push({
+          $addFields: {
+               picture: {
+                    $arrayElemAt: ['$pictures', 0]
+               },
+               pictures: null,
+          }
+     })
+     let journals = await models.travelJournal.aggregate(aggregate)
+     res.json({
+          journals
+     })
+})
 
 module.exports = router
